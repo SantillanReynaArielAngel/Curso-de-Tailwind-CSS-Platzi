9a79318791f79e3d4ea63c6de83e539330b11f14
Santillan Reyna Ariel Angel <santillanreynaarielangel1@gmail.com>
2021-04-04 16:34:45 -0400

    ResponsiveDesing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    //Cambairemos al tipografia que esta por defecto
+    fontFamily:{
+      display:['Gilroy', 'sans-serof'],
+      body:['Graphik', 'sans-serif']
+    },
+    extend: {
+      //Añadiremos un nuevo color a la paleta de tailwind
+      colors:{
+        cyan:'#9cdbff',
+      }
+    },
   },
   variants: {
     extend: {},
