5930864980233bea4e117df96955f32e286f94eb
Santillan Reyna Ariel Angel <santillanreynaarielangel1@gmail.com>
2021-04-04 17:01:40 -0400

    Colores_Video06

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    //Cambairemos al tipografia que esta por defecto
-    fontFamily:{
-      display:['Gilroy', 'sans-serof'],
-      body:['Graphik', 'sans-serif']
-    },
-    extend: {
-      //Añadiremos un nuevo color a la paleta de tailwind
-      colors:{
-        cyan:'#9cdbff',
-      }
-    },
+    extend: {}
   },
   variants: {
     extend: {},
