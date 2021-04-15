f9897a53580821a0121e372a9108dccaf56da57e
Santillan Reyna Ariel Angel <santillanreynaarielangel1@gmail.com>
2021-04-15 11:55:59 -0400

    HeaderPlatziFoot_AñadirColoresPropiosYOtros

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      // De esta manera añadimos colores propios 
+      colors:{
+      'primary':'#f3c614',
+      'secundary':'#353535',
+    }
+  }
   },
   variants: {
     extend: {
