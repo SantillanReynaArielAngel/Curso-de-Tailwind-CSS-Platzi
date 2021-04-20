e398b62b72040adbf2cd2242467180eb73b1e9c1
Santillan Reyna Ariel Angel <santillanreynaarielangel1@gmail.com>
2021-04-20 16:02:40 -0400

    CursoFinalizado

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,13 @@ module.exports={
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        // Lo siguiente elimina el codigo css no utilizado de style.css 
         purgecss({
             content: ['./**/*.html']
-          })
+          }),
+        // Lo siguiente comprime(elimina saltos de linea y espacios) de un css 
+        require('cssnano')({
+            preset: 'default',
+          }),
     ]
 } 
